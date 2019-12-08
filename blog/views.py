189ff4ae14edbca05415aa8eba6f4be7112e4c4f
Yanky Hoffman <developer.yankyhoffman@gmail.com>
2019-12-08 17:29:14 -0500

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from .models import Post
+
+
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', {'posts': posts})
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
